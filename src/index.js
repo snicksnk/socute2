79ab489165cfca0f2b544b680e4a3a99d8fc1f39
Your Name <you@example.com>
2016-05-19 20:13:35 +0400

    two

--- src/index.js
+++ src/index.js
@@ -2,6 +2,43 @@ var co = require('co');
 var $ = require('jquery');
 
 
+console.log('22 2  --33 - assaas');
+
+
+
+
+function calculateMouseDiff(state){
+	state.diff = [state.mouse[0] - state._prevMouse[0], state.mouse[1] - state._prevMouse[1]];
+	return state;
+}
+
+
+function updateMousePrev(state){
+	state._prevMouse = state.mouse;
+	return state;
+}
+
+function setSelectedNode(state, node){
+	state.selected = node;
+	console.log(state);
+	return state;
+}
+
+function moveSelectedNode(state){
+
+	moveNode(state.selected, state.mouse);
+	return state;
+}
+
+function moveNode(node, coords){
+	var element = $('#'.node);
+	console.log(node, coords, element);
+	element.attr('x', coords[0]);
+	element.attr('y', coords[1]);
+}
+
+console.log('init5');
+
 $().ready(() => {
 
 
@@ -14,36 +51,28 @@ $().ready(() => {
 	};
 
 
-	function calculateMouseDiff(state){
-		state.diff = [state.mouse[0] - state._prevMouse[0], state.mouse[1] - state._prevMouse[1]];
-		return state;
-	}
-
 	$('body').mousemove(event => {
 		state.mouse = [event.pageX, event.pageY];
 		//console.log(state.mouse);
 	});
 
+	$('.node').click(e => {
+		var id = e.target.id;
+		state = setSelectedNode(state, id);
+	});
+
 
 	var fps = (lastValue) => new Promise(resolve => {
-		console.log('fps')
-		setTimeout(() => {resolve(true);}, 500);
+		
+		setTimeout(() => {resolve(true);}, 1000);
 	});
 
 	co(function * () {
-	  console.log('---==-')
 	  	
-	  	var rect = $('#rect');
 		while (yield fps()){
-
 			state = calculateMouseDiff(state);
-
-			console.log(state.mouse, state.diff);
-
-			rect.attr('x', state.mouse[0]);
-			rect.attr('y', state.mouse[1]);
-
-			state._prevMouse = state.mouse;
+			state = moveSelectedNode(state);
+			state = updateMousePrev(state);
 		}
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,12 @@
 var path = require('path')
 var webpack = require('webpack')
-var NpmInstallPlugin = require('npm-install-webpack-plugin')
+var NpmInstallPlugin = require('npm-install-webpack-plugin');
 var autoprefixer = require('autoprefixer');
 var precss = require('precss');
 
 module.exports = {
   devtool: 'source-map',
   entry: [
-    'webpack-hot-middleware/client',
     'babel-polyfill',
     './src/index'
   ],
@@ -16,28 +15,39 @@ module.exports = {
     filename: 'bundle.js',
     publicPath: '/static/'
   },
+ 
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
     new NpmInstallPlugin()
   ],
-  module: {
+ module: {
     loaders: [
       {
         loaders: ['react-hot', 'babel-loader'],
         include: [
-          path.resolve(__dirname, "src"),
+          path.resolve(__dirname, "public"),
         ],
         test: /\.js$/,
         plugins: ['transform-runtime'],
       },
       {
-        test:   /\.css$/,
-        loader: "style-loader!css-loader!postcss-loader"
+        test:   /\.scss$/,
+        loader: "style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!sass-loader"
+      },
+      {
+       test: /\.(jpe?g|png|gif|svg)$/i,
+       loader: 'url?limit=10000!img?progressive=true'
+      },
+      {
+        test   : /\.(ttf|eot|woff(2)?)(\?[a-z0-9]+)?$/,
+        loader : 'file-loader'
       }
     ]
   },
   postcss: function () {
     return [autoprefixer, precss];
-  }
+  },
+  watch: true
 }
